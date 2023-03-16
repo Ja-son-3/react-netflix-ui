fe36583e21067e0ff29e7e9af4506565afb435bb
Jason Huang <jason97521@gmail.com>
2023-03-16 05:46:27 -0400

    react login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import Home from "./pages/home/Home";
 import "./app.scss"
 import Watch from "./pages/watch/Watch";
 import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 
 function App() {
   return (
-    <Register/>
+    <Home/>
   );
 }
 
