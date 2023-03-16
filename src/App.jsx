10643310ff3bf806964900100f00f3bc29bf359a
Jason Huang <jason97521@gmail.com>
2023-03-16 05:28:11 -0400

    react register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Home from "./pages/home/Home";
 import "./app.scss"
 import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Register";
 
 
 function App() {
   return (
-    <Watch/>
+    <Register/>
   );
 }
 
